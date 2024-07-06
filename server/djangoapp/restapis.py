70550841be01979684ee1e938e23c611bb80ae7a
Dudeyay <125753648+Dudeyay@users.noreply.github.com>
2024-07-07 01:57:48 +0800

    Update restapis.py
    Fixed linting error

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -14,6 +14,7 @@ sentiment_analyzer_url = os.getenv(
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 
+
 def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
@@ -29,7 +30,7 @@ def get_request(endpoint, **kwargs):
         return response.json()
     except Exception as e:
         # If any error occurs
-        print("Network exception occurred")
+        print(f"Error: {e}")
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
@@ -53,5 +54,5 @@ def post_review(data_dict):
         print(response.json())
         return response.json()
     except Exception as e:
-        print("Network exception occurred")
+        print(f"Error: {e}")
 # Add code for posting review
